414d93a9672dd2feba673cf24aba585146d48d89
Marina <marinaskripnik97@gmail.com>
2016-11-19 21:22:53 +0200

    Neil Gaiman

--- js/texttab.js
+++ js/texttab.js
@@ -1,24 +1,34 @@
 $(function(){
+
 	let url = $(location).attr('href'),
 	href = url.substr(url.length - 2);
-	console.log($('a.is-active').attr('href'));
-	// $('#tabs').find('a').each(function(){
-	// 	if (this.href === href) {
-	// 		$(this).addClass('is-active');
-	// 	}
-	// });
+
+	function tab(link, img){
+		$('.contentwrap').find('div.is-active').removeClass('is-active');
+		$('#tabs').find('a.is-active').removeClass('is-active');
+		link.addClass('is-active');
+		$('.contentwrap').find('div.tab').eq(link.index()-1).addClass('is-active');
+		$('.hero').css('background-image', img);
+	}
+
+	function img(){
+		href = href.substr(href.length-1)-1;
+		let img = "url('img/img"+href+".jpg";
+		return img;
+	}
+
+	$('#tabs').find('a').each(function() {
+		if ($(this).attr('href') == href) {
+			tab($(this), img());
+		}
+	});
 
 	$('#tabs a').click(function(){
 		url = $(this).attr('href');
 		href = url.substr(url.length - 1);
-		console.log(href);
-		if (href >= 2) {
-			if ($(this).attr('class') !== 'is-active') {
-				$('.contentwrap').find('div.is-active').removeClass('is-active');
-		    	$('#tabs').find('a.is-active').removeClass('is-active');
-		    	$(this).addClass('is-active');
-		    	$('.contentwrap').find('div.tab').eq($(this).index()-1).addClass('is-active');
-			}
+		if ($(this).attr('class') !== 'is-active') {
+			tab($(this), img());
 		}
 	});
+
 });
