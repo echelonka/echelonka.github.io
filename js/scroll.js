6c80496481466217923bd23dbcd38a5b13aaee4f
echelonka <marinaskripnik97@gmail.com>
2017-06-20 14:40:24 +0300

    Neil Gaiman

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,15 @@
 $(function() {
-	$(window).scroll(function() {
-		if($(this).scrollTop() > 300) {
-			$('.top').fadeIn();
-		} else {
-			$('.top').fadeOut();
-		}
-	});
-	$('.top').css('opacity', .5);
-	$('.top').click(function() {
-		$('body,html').animate({scrollTop:0},500);
-	});
-	$('.top').hover(function(){
-		$(this).animate({opacity: 1},200);
-		},
-		function(){
-			$(this).animate({opacity: .5},200);
-		});
+	$(window).scroll(
+		() => ($(this).scrollTop() > 300) ? $('.top').fadeIn() : $('.top').fadeOut());
+	$('.top').css('opacity', .5)
+		.click(function() {
+			$('body,html').animate({scrollTop: 0}, 500);
+		})
+		.hover(function(){
+				$(this).animate({opacity: 1}, 200);
+			},
+			function(){
+				$(this).animate({opacity: .5}, 200);
+			}
+		);
 });
--- js/texttab.js
+++ js/texttab.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	var url = $(location).attr('href'),
+	let url = $(location).attr('href'),
 	href = url.substr(url.length - 2);
 
 	function tab(link, img){
@@ -8,27 +8,22 @@ $(function(){
 		$('#tabs').find('a.is-active').removeClass('is-active');
 		link.addClass('is-active');
 		$('.contentwrap').find('div.tab').eq(link.index()-1).addClass('is-active');
-		$('.hero').css('background-image', "url('"+img+"')");
+		$('.hero').css('background-image', `url('${img}')`);
 	}
 
 	function img(){
 		href = href.substr(href.length-1)-1;
-		var img = "img/img"+href+".jpg";
+		let img = `img/img${href}.jpg`;
 		return img;
 	}
 
-	$('#tabs').find('a').each(function() {
-		if ($(this).attr('href') == href) {
-			tab($(this), img());
-		}
-	});
-
+	$("#tabs a").find("a").each(() => ($(this).attr("href") == href) ? tab($(this), img) : false);
 	$('#tabs a').click(function(){
 		url = $(this).attr('href');
 		href = url.substr(url.length - 1);
 		if ($(this).attr('class') !== 'is-active') {
 			tab($(this), img());
-			$('body,html').animate({scrollTop:0},0);
+			$('body, html').animate({scrollTop: 0}, 0);
 		}
 	});
 
